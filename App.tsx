0f9adf25187f11fb3316ba81b71101ddee8a745c
parsa_j42 <parsajfri42@gmail.com>
2024-10-11 19:51:45 -0600

    Push lab files

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import ToDoList from './components/ToDoList/ToDoList';
+import ToDoForm from './components/ToDoForm/ToDoForm';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView style={styles.container}>
+        <ToDoList />
+        <ToDoForm />
+        <StatusBar style="auto" />
+      </SafeAreaView>
   );
 }
 
@@ -14,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

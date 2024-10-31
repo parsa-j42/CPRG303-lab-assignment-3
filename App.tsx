e6b515095abebbdb20873e8db9e63c6fc38c080b
parsa_j42 <parsajfri42@gmail.com>
2024-10-30 23:15:44 -0600

    Assignment 3

--- App.tsx
+++ App.tsx
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import ToDoList from './components/ToDoList/ToDoList';
 import ToDoForm from './components/ToDoForm/ToDoForm';
 
 export default function App() {
-  return (
-      <SafeAreaView style={styles.container}>
-        <ToDoList />
-        <ToDoForm />
-        <StatusBar style="auto" />
-      </SafeAreaView>
-  );
+    const [tasks, setTasks] = useState<string[]>([
+        'Do laundry',
+        'Go to gym',
+        'Walk dog'
+    ]);
+
+    return (
+        <SafeAreaView style={styles.container}>
+            <ToDoList tasks={tasks} />
+            <ToDoForm />
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
 });
